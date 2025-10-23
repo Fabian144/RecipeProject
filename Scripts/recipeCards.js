0b0a069184370ab599ae7d1bfcd2203b800287a4
Fabian144 <fabianyousif24@gmail.com>
2025-10-23 15:59:12 +0200

    More styling, added script element for usage of icons

--- Scripts/recipeCards.js
+++ Scripts/recipeCards.js
@@ -2,30 +2,35 @@ import { recipes } from "./main.js";
 
 let recipeCardHTML = ``;
 
-console.log(recipes)
 recipes.forEach((recipe) => {
   recipeCardHTML += `<a class="recipe-card-link" href="./recipePage.html">
 
-			<article class="recipe-card">
-				<figure class="recipe-image-container">
-					<img src="${recipe.image}" alt="${recipe.alt_text}">
-				</figure>
+		<article class="recipe-card">
+			<figure class="recipe-image-container">
+				<img src="${recipe.image}" alt="${recipe.alt_text}">
+			</figure>
 
-				<div class="recipe-info-container">
-					<h2 class="recipe-heading">
-						${recipe.name}
-					</h2>
+			<div class="recipe-info-container">
+				<h2 class="recipe-heading">
+					${recipe.name}
+				</h2>
 
-					<p class="recipe-description">
-						${recipe.description}
-					</p>
-
-					<hr>
-					<p class="recipe-details">${recipe.ingredients.length} INGREDIENSER | ${recipe.cooking_time} MINUTER</p>
-
+				<div class="recipe-star-container">
 				</div>
-			</article>
-		</a>`;
+
+				<p class="recipe-description">
+					${recipe.description}
+				</p>
+
+			</div>
+
+			<div class="recipe-detail-container">
+				<hr>
+				<p class="recipe-details">${recipe.ingredients.length} INGREDIENSER | ${recipe.cooking_time} MINUTER</p>
+			</div>
+			
+		</article>
+	</a>`;
 });
 
 document.body.innerHTML += recipeCardHTML;
